cbbac61ae2eab4c5e947e3b97e67528fdd6f7352
lusine duryan <lusineduryan@mail.ru>
2020-06-15 02:31:09 +0400

    vv

--- LoginTest/Blog.UITests/BlogWebAppShould.cs
+++ LoginTest/Blog.UITests/BlogWebAppShould.cs
@@ -38,7 +38,6 @@ namespace Blog.UITests
                 //IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]"));
                 IWebElement element = driver.FindElement(By.Id("vuukle-emote"));
                 element.Click();
-                
                 DemoHelper.Pause();
                 
                 driver.Navigate().Refresh();
